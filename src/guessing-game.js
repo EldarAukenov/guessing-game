a3fccc6b4ae76efa1f0a9199c38a2349b4e09d6d
EldarAukenov <119871117+EldarAukenov@users.noreply.github.com>
2023-02-25 17:37:37 +0600

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,26 +1,27 @@
 class GuessingGame {
-  constructor() {
-    this.min = null;
-    this.max = null;
-    this.guessNum = null;
-  }
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.middle = 0;
+    }
 
-  setRange(min, max) {
-    this.min = min;
-    this.max = max;
-  }
+    setRange(min, max) {
+        this.min = min;
+        this.max = max;
+    }
 
-  guess() {
-    this.guessNum = Math.floor((this.min + this.max) / 2);
-    return this.guessNum;
-  }
+    guess() {
+        this.middle = Math.round((this.max+this.min)/2);
+        return this.middle;
+    }
 
-  lower() {
-    this.max = this.guessNum;
-  }
+    lower() {
+      this.max = this.middle;
+    }
 
-  greater() {
-    this.min = this.guessNum;
-  }
+    greater() {
+      this.min = this.middle;
+    }
 }
+
 module.exports = GuessingGame;
